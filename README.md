12adea416893b64c26613dd985f4f39eacd1c28d
Daniel Spitzer <dsp@tradeshift.com>
2013-03-04 20:27:31 +0100

    War Stories!

--- README.md
+++ README.md
@@ -2,3 +2,8 @@ warstories-spma
 ===============
 
 Our presentation about Tradeshift, Responsive design and AngularJS/SpiritualJS on how we are building the world's best business platform!
+
+_Run ./serve.sh or view template.html in your browser to view the presentation!_
+
+
+*Presentation framework is heavily based on https://code.google.com/p/io-2012-slides*
---
+++ slide_config.js
@@ -0,0 +1,34 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'War stories',
+    subtitle: 'Going all-in on responsive design & SP(M)A',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/favicon.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Droid Sans Mono'
+    ],
+    theme: [], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Rolf Njor Jensen',
+    company: 'Code Monkey, Tradeshift',
+    gplus: 'http://plus.google.com/111034247972055596652',
+    twitter: '@rolfnjorjensen',
+    github: 'http://github.com/rolfnjorjensen'
+  }, {
+    name: 'Daniel Spitzer',
+    company: 'Web Magician Extraordinaire, Tradeshift',
+    gplus: 'http://plus.google.com/106623924287099254159',
+    twitter: '@dnsp',
+    github: 'http://github.com/sampi'
+  }]
+};
+
